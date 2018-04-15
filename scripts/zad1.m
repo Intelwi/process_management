06da649a3057599aa04ce8250742ea368c314d9b
Micha <mlk04@wp.pl>
2018-04-15 14:35:02 +0200

    zad4

--- scripts/zad1.m
+++ scripts/zad1.m
@@ -1,15 +1,13 @@
+
 %Michał Stolarz
 %Zad nr 1
+
+% Ciągłe ----------------------------------------
+
 Numerator = [0 1 9 8];
 Denominator = [1 12 -79 -990];
 sys = tf(Numerator,Denominator) % transmitancja ciągła
 
-Ts = 0.25; % czas próbkowania
-
-sysd = c2d(sys,Ts,'zoh') % transmitancja dyskretna, otrzymana przez ektrapolator zerowego rzędu
-
-a =pole(sysd) % bieguny transmitancji dyskretnej
-b = zero(sysd) % zera transmitancji dyskretnej
 [A,B,C,D] = tf2ss(Numerator,Denominator) %1 wariant metody bezpośredniej
 
 % współczynniki dla 2 wariantu metody bezpośredniej
@@ -17,3 +15,20 @@ A1=A.'
 B1 = C.'
 C1 = B.'
 
+% Dyskretne --------------------------------------
+
+Ts = 0.25; % czas próbkowania
+
+sysd = c2d(sys,Ts,'zoh') % transmitancja dyskretna, otrzymana przez ektrapolator zerowego rzędu
+
+a =pole(sysd) % bieguny transmitancji dyskretnej
+b = zero(sysd) % zera transmitancji dyskretnej
+
+[num,den] = tfdata(sysd)
+
+[A2,B2,C2,D2] = tf2ss(num{1},den{1}) %1 wariant metody bezpośredniej
+
+% współczynniki dla 2 wariantu metody bezpośredniej
+A3=A2.'
+B3 = C2.'
+C3 = B2.'
