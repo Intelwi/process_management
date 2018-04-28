9185d2e999532f9797e8b61d2c63bb57e77ac979
Micha <mlk04@wp.pl>
2018-04-28 21:05:18 +0200

    10zad

--- scripts/zad8.m
+++ scripts/zad8.m
@@ -30,21 +30,22 @@ C3 = B2.'
 [b2,a2] = ss2tf(A3,B3,C3,D2) % przestrzń stanów dla drugiej metody
 
 % Regulator
-a=0.5%0.3
-b=0.6%0.2
+a=0.3
+b=0.1
 z1 = a + b*j
 z2 = a - b*j
 
-k=0.2
-K = acker(A3,B3,[k k k])
-
+k=0.3
+K = acker(A3,B3,[k z1 z2])
+%K = acker(A3,B3,[k k k])
 %obserwator
-l1=0.2
+l1=0.6
+l2=0.6
 A3_11=A3(1,1)
 A3_21=A3(2:3,1)
 A3_22 = A3(2:3,2:3)
 A3_12 = A3(1,2:3)
 B3_1 = B3(1)
 B3_2 = B3(2:3)
-L = acker(A3_22',A3_12',[l1 l1])% obliczenie parametrów obserwatora
+L = acker(A3_22',A3_12',[l1 l2])% obliczenie parametrów obserwatora
 L=L'
