fd504fd8a9447147eaa496ba191b28b8cf1be17c
Micha <mlk04@wp.pl>
2018-05-13 20:36:52 +0200

    Ziegler and Nicholson

--- projekt2/zad1/zad1.m
+++ projekt2/zad1/zad1.m
@@ -10,8 +10,10 @@ T_2 = 5.27
 s = tf('s')
 sys = K_0* exp(-T_0*s)/((s*T_1+1)*(s*T_2+1)) % transmitancja
 
+opt = stepDataOptions('InputOffset',0,'StepAmplitude',1); % ustawienia skoku
+
 subplot(2,1,1)
-step(sys,35) % odpowiedź skokowa
+step(sys,35,opt) % odpowiedź skokowa
 
 K_c = dcgain(sys) % wzmocnienie statyczne
 
@@ -23,7 +25,7 @@ Tp = 0.5; % czas próbkowania
 sysd = c2d(sys,Tp,'zoh') % transmitancja dyskretna, otrzymana przez ektrapolator zerowego rzędu
 
 subplot(2,1,2)
-step(sysd,35) % odpowiedź skokowa
+step(sysd,35,opt) % odpowiedź skokowa
 
 K_d = dcgain(sysd) % wzmocnienie statyczne
 
--- projekt2/zad1/zad2.m
+++ projekt2/zad1/zad2.m
@@ -7,13 +7,19 @@ T_2 = 5.27
 s = tf('s')
 sys = K_0* exp(-T_0*s)/((s*T_1+1)*(s*T_2+1)) % transmitancja
 
+%Regulator PID
+
+Kk = 0.4802 % Kp=0.4802 <- wzmocnienie krytyczne Tk = 20 sek  <- okres oscylacji
+Tk = 20
+
 %################################################
-Kp=0.4802  % Kp=0.4802 <- wzmocnienie krytyczne
-Ki=0
-Kd=0
+Kp=0.6*Kk  
+Ki=1/(0.5*Tk)
+Kd=0.12*Tk
 %################################################
 
 Pid = pid(Kp,Ki,Kd) % regulator
 
 T = feedback(Pid*sys,1) % sprzężenie zwrotne
-step(T) % skok
+opt = stepDataOptions('InputOffset',0,'StepAmplitude',1); % ustawienia skoku
+step(T,50,opt) % skok
