ebbe0c63a241cb056b703bb1ebca783551abad0d
Micha <mlk04@wp.pl>
2018-05-26 09:20:27 +0200

    sprawozdanie

--- projekt2/zad1/zad1.m
+++ projekt2/zad1/zad1.m
@@ -13,7 +13,10 @@ sys = (4.7*exp(-5*s))/((s*1.89+1)*(s*5.27+1)) % transmitancja
 opt = stepDataOptions('InputOffset',0,'StepAmplitude',1); % ustawienia skoku
 
 subplot(2,1,1)
-step(sys,35,opt) % odpowiedź skokowa
+step(sys,50,opt) % odpowiedź skokowa
+title("Odpowiedź skokowa G(s)");
+xlabel("Czas[s]");
+ylabel("Amplituda")
 
 K_c = dcgain(sys) % wzmocnienie statyczne
 
@@ -25,6 +28,10 @@ Tp = 0.5; % czas próbkowania
 sysd = c2d(sys,Tp,'zoh') % transmitancja dyskretna, otrzymana przez ektrapolator zerowego rzędu
 
 subplot(2,1,2)
-step(sysd,35,opt) % odpowiedź skokowa
+step(sysd,50,opt) % odpowiedź skokowa
+title("Odpowiedź skokowa G(z)");
+xlabel("Czas[s]");
+ylabel("Amplituda")
+
 
 K_d = dcgain(sysd) % wzmocnienie statyczne
