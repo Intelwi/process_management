46efd79cfbcb169f2d6edd6a12392d28131293b0
Micha <mlk04@wp.pl>
2018-05-31 17:56:35 +0200

    zad6 plot

--- projekt2/zad6/zad6_DMC.m
+++ projekt2/zad6/zad6_DMC.m
@@ -1,42 +1,52 @@
 %Michał Stolarz
-%Zad nr 5
+%Zad nr 6
+%Testy dla algorytmu DMC
 
-%##########Odpowiedź skokowa obiektu###############
 
-kk=200 %koniec symulacji 
+%##############Obiekt####################
+k0=2.2275;%K_0/K_0_nom
+t=2; %T_0/T_0_nom
 
-x=1:kk
+K_0 = 4.7;
+T_0 = 5;
+T_1 = 1.89;
+T_2 = 5.27;
+
+% Ciągłe ----------------------------------------
+S = tf('s');
+sys = k0*K_0* exp(-t*T_0*S)/((S*T_1+1)*(S*T_2+1)); % transmitancja ciągła
+%Dyskretne --------------------------------------
+Tp=0.5;
+sysd = c2d(sys,Tp,'zoh'); % transmitancja dyskretna
+
+[num,den] = tfdata(sysd,'v'); % pobranie współczynników
+delay = get(sysd,'OutputDelay'); % pobranie opóźnienia
+
+%##############Regulator#################
+kk=4000; %koniec symulacji 
+
+x=1:kk;
 y=zeros(kk,1);
-u=zeros(kk,1);
-
-%warunki pocztkowe 
-u(1:12)=1; 
-y(1:12)=0; 
-u(1:12)=1; 
-u(13:kk)=1;  
+u=ones(kk,1);
 
 for k=13:kk; %główna petla symulacyjna 
 %symulacja obiektu 
      y(k)=1.677*y(k-1)-0.6981*y(k-2)+0.0524*u(k-11)+0.04649*u(k-12); 
 end; 
 
-
 % Wektor z rzędnymi odpowiedzi skokowych obiektu
-
 s = y;
 
 % Horyzonty
-D=80
+D=80;
+N=18;
+Nu=1;
 
-N=18
-Nu=1
 % Współczynnik kary za przyrosty sterowania
-
 lambda=400;%dla 400 jest ok 
 %zbadać dla 100, 1000, 500, 400, 300
 
 % Generacja macierzy
-
 M=zeros(N,Nu);
 for i=1:N
    for j=1:Nu
@@ -58,7 +68,6 @@ for i=1:N
 end;
 
 % Obliczanie parametrów regulatora
-
 I=eye(Nu);
 K=((M'*M+lambda*I)^-1)*M';
 
@@ -68,9 +77,9 @@ d_u=zeros(N,1);
 d_u_stare = zeros(D,1);
 u = zeros(kk,1);
 
-for k=13:kk; %główna petla symulacyjna 
+for k=(delay+3):kk; %główna petla symulacyjna 
    
-   y_model(k)=1.677*y_model(k-1)-0.6981*y_model(k-2)+0.0524*u(k-11)+0.04649*u(k-12); %odpowiedź modelu na sterowanie 
+   y_model(k)=(1/(den(1))) * ((-den(2))*y_model(k-1)+(-den(3))*y_model(k-2)+num(2)*u(k-(delay)-1)+num(3)*u(k-(delay)-2)); %odpowiedź modelu na sterowanie 
   
    d_u = K*(y_zad(1)*ones(N,1)- y_model(k)*ones(N,1) -MP*d_u_stare(1:D-1)); %zmiana sterowania
    
@@ -82,14 +91,15 @@ for k=13:kk; %główna petla symulacyjna
 end
 
 %wartość zadana
-figure
-hold on
+figure;
+hold on;
 stairs(y_model);
-stairs(y_zad) 
-title("y, y_{zad}")
+stairs(y_zad) ;
+title("y, y_{zad}");
 ylabel("Amplitude");
 xlabel("k");
-hold off
+hold off;
+
 
 %sterowanie
 figure
--- projekt2/zad6/zad6_PID.m
+++ projekt2/zad6/zad6_PID.m
@@ -4,35 +4,35 @@
 
 
 %##############Obiekt####################
-k=1,4%K_0/K_0_nom
-t=1 %T_0/T_0_nom
+k=1;%K_0/K_0_nom
+t=1;%T_0/T_0_nom
 
-K_0 = 4.7
-T_0 = 5
-T_1 = 1.89
-T_2 = 5.27
+K_0 = 4.7;
+T_0 = 5;
+T_1 = 1.89;
+T_2 = 5.27;
 
 % Ciągłe ----------------------------------------
-s = tf('s')
-sys = k*K_0* exp(-t*T_0*s)/((s*T_1+1)*(s*T_2+1)) % transmitancja ciągła
+s = tf('s');
+sys = k*K_0* exp(-t*T_0*s)/((s*T_1+1)*(s*T_2+1)); % transmitancja ciągła
 %Dyskretne --------------------------------------
-Tp=0.5
-sysd = c2d(sys,Tp,'zoh') % transmitancja dyskretna
+Tp=0.5;
+sysd = c2d(sys,Tp,'zoh'); % transmitancja dyskretna
 
-[num,den] = tfdata(sysd,'v') % pobranie współczynników
+[num,den] = tfdata(sysd,'v'); % pobranie współczynników
 delay = get(sysd,'OutputDelay'); % pobranie opóźnienia
 
 %##############Regulator#################
-Kk = 0.4802 % Kp=0.4802 <- wzmocnienie krytyczne 
-Tk = 20 % Tk = 20 sek  <- okres oscylacji
+Kk = 0.4802; % Kp=0.4802 <- wzmocnienie krytyczne 
+Tk = 20; % Tk = 20 sek  <- okres oscylacji
 
-Kp=0.6*Kk
-Ti=(0.5*Tk)
-Td=0.12*Tk
+Kp=0.6*Kk;
+Ti=(0.5*Tk);
+Td=0.12*Tk;
 
-r0 = Kp*(1+(Tp/(2*Ti))+(Td/Tp))
-r1 = Kp*(-1-2*(Td/Tp)+Tp/(2*Ti))
-r2 = (Kp*Td)/Tp
+r0 = Kp*(1+(Tp/(2*Ti))+(Td/Tp));
+r1 = Kp*(-1-2*(Td/Tp)+Tp/(2*Ti));
+r2 = (Kp*Td)/Tp;
 
 %dyskretny obiekt
 
@@ -50,7 +50,7 @@ x=1:kk
 y=zeros(kk,1);
 u=zeros(kk,1);
 e=zeros(kk,1);
-yzad = ones(kk,1)
+yzad = ones(kk,1);
 
 %warunki pocztkowe 
 u(1:delay+2)=0; 
@@ -86,6 +86,3 @@ title("y, y_{zad}")
 ylabel("Amplitude");
 xlabel("k") 
 hold off
-
-
-sysd
